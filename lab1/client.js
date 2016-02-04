dd407d9f87040b05df14a9bb34fbeb445d899cb9
Drakenhielm <fbrolund@gmail.com>
2016-02-04 17:49:00 +0100

    Added profileView to client

--- lab1/client.js
+++ lab1/client.js
@@ -69,3 +69,27 @@ signupValidation = function(form){
 	
 	return true;
 }
+
+/*Profileview*/
+tabClick = function(tab){
+	
+	console.log("Tab clicked");
+	var home = document.getElementById("Home");
+	var browse = document.getElementById("Browse");
+	var account = document.getElementById("Account");
+	
+	home.style.display = "none";
+	browse.style.display = "none";
+	account.style.display = "none";
+
+	if(tab == Home) {
+		home.style.display = "block";
+	} else if(tab == Browse) {
+		browse.style.display = "block";
+	} else if(tab == Account) {
+		account.style.display = "block";
+	}else{
+		console.log("Error in tabClick()");
+	}
+		
+}
