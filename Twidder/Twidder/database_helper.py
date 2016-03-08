2b3e8e633b20818ef14edefd943d9dc58dc838a6
iawls <falken1994@hotmail.com>
2016-03-08 18:51:27 +0100

    Lab3 stuffz, more inc

---
+++ Twidder/Twidder/database_helper.py
@@ -0,0 +1,85 @@
+import sqlite3 as lite
+from flask import Flask, g
+
+dbhelper = Flask(__name__)
+
+
+def connect_db():
+    return lite.connect("database.db")
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = g.db = connect_db()
+    return db
+
+def init_db():
+    with dbhelper.app_context():
+        db = get_db()
+        with dbhelper.open_resource('database.schema', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit();
+
+def add_message(name, message):
+    c.get_db()
+    c.execute("insert into entries (name,message) values (?,?)", (name,message))
+    c.commit()
+
+def close():
+    get_db().close()
+
+def get_message(name):
+    c = get_db()
+    cursor = c.cursor()
+    cursor.execute("select name, message from entries where name = '" + name + "'")
+    entries = [dict(name=row[0], message=row[1]) for row in cursor.fetchall()]
+    c.close()
+    return entries[0]['name'] + " says: " + entries[0]['message']
+
+def query_db(query, args=(), one=False):
+    with dbhelper.app_context():
+        print query
+        c = get_db().execute(query, args)
+        get_db().commit()
+        response = c.fetchall()
+        c.close()
+        return (response[0] if response else None) if one else response
+
+def add_user(email, password, fname, lname, gender, city, country):
+    resp = query_db("INSERT INTO users VALUES (?,?,?,?,?,?,?)", [email, password, fname, lname, gender, city, country])
+    return resp
+
+def get_user_by_email(email):
+    resp = query_db("SELECT * FROM users WHERE email=?", [email])
+    return resp
+
+def get_user_by_token(token):
+    resp = query_db("SELECT email FROM loggedInUsers WHERE token=?", [token])
+    return resp
+
+def login(email, token):
+    resp = query_db("INSERT INTO loggedInUsers VALUES (?,?)", [email, token])
+    return resp
+
+def logout(token):
+    resp = query_db("DELETE FROM loggedInUsers WHERE token=?", [token])
+    return resp;
+
+def change_password(email, password):
+    resp = query_db("UPDATE users SET password=? WHERE email=?", [password, email])
+    return resp;
+
+def post_message(sender, reciever, message):
+    resp = query_db("INSERT INTO messages VALUES (?,?,?)", [sender, reciever, message])
+    return resp
+
+def get_messages(user):
+    resp = query_db("SELECT * FROM messages WHERE reciever=?", [user])
+    return resp
+
+def get_user_data(user):
+    resp = query_db("SELECT * FROM users WHERE email=?", [user])
+    return resp
+
+
+
---
+++ Twidder/Twidder/server.py
@@ -0,0 +1,165 @@
+from flask import Flask, jsonify, request
+from Twidder import app
+import database_helper
+from random import randint
+
+
+@app.route("/")
+def root():
+    return app.send_static_file("client.html")
+
+
+@app.route("/socket")
+def forcelogout():
+    return
+
+
+@app.route("/signin", methods=['POST'])
+def sign_in():
+    email = request.values.get('email')
+    password = request.values.get('password')
+    user = database_helper.get_user_data(email)
+
+    if len(user) <= 0:
+        return jsonify(success = False, message = "Wrong username or password")
+    else:
+        if user[0][1] == password:
+            characters = "abcdefghiklmnopqrstuvwwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+            token = ""
+            for i in range(0, 35):
+                token += characters[randint(0, len(characters)-1)]
+            
+            database_helper.login(email, token)
+            return jsonify(success = True, message = "Successfully signed in.", data = token)
+        else:
+            return jsonify(success = False, message = "Wrong username or password.")
+
+
+
+@app.route("/signout", methods=['POST'])
+def sign_out():
+    token=request.values.get('token')
+    user = database_helper.get_user_by_token(token)
+    if(len(user) == 0):
+        return jsonify(success = False, message = "You are not signed in")
+    else:
+        resp=database_helper.logout(token)
+        return jsonify(success = True, message = "Successfully logged out")
+
+@app.route("/signup", methods=['POST'])
+def sign_up():
+    email=request.values.get('email')
+    password=request.values.get('password')
+    fname=request.values.get('fname')
+    lname=request.values.get('lname')
+    gender=request.values.get('gender')
+    city=request.values.get('city')
+    country=request.values.get('country')
+
+    tmp = database_helper.get_user_data(email)
+
+    if(email == None or password == None or fname == None or lname == None or gender == None or city == None or country == None):
+        return jsonify(success = False, message = "Form data missing")
+    elif len(tmp) > 0:
+        return jsonify(success = False, message = "User already exists")
+    else:
+        database_helper.add_user(email, password, fname, lname, gender, city, country)
+        return jsonify(success = True, message = "Successfully created a new user")
+
+@app.route("/changepassword", methods=['POST'])
+def change_password():
+    token = request.values.get('token')
+    old_pw = request.values.get('old_password')
+    new_pw = request.values.get('new_password')
+    
+    user = database_helper.get_user_by_token(token)
+    if len(user) <= 0:
+        return jsonify(success = False, message = "You are not logged in")
+
+    userpw = database_helper.get_user_by_email(user[0][0])
+
+    if old_pw == userpw[0][1]:
+        database_helper.change_password(user[0][0], new_pw)
+        return jsonify(success = True, message = "Password changed")
+    else:
+        return jsonify(success = False, message = "Wrong password")
+    
+
+@app.route("/getuserdatabytoken", methods=['GET'])
+def get_user_data_by_token():
+    token = request.values.get('token')
+    user = database_helper.get_user_by_token(token)
+    
+    if len(user) > 0:
+        userdata = database_helper.get_user_by_email(user[0][0])
+        if len(userdata) > 0:
+            return jsonify(success = True, message = "Data successfully got", data = [userdata[0][0], userdata[0][2], userdata[0][3], userdata[0][4], userdata[0][5], userdata[0][6]])
+        else:
+            return jsonify(success = False, message = "Something went terribly wrong, this shouldn't happen")
+
+    else:
+        return jsonify(success = False, message = "You're not logged in")
+
+@app.route("/getuserdatabyemail", methods=['GET'])
+def get_user_data_by_email():
+    email = request.values.get('email')
+    token = request.values.get('token')
+    userFromToken = database_helper.get_user_by_token(token)
+    if len(userFromToken) > 0:
+        userdata = database_helper.get_user_by_email(email)
+        if len(userdata) > 0:
+            return jsonify(success = True, data = [userdata[0][0], userdata[0][2], userdata[0][3], userdata[0][4], userdata[0][5], userdata[0][6]], message = "Successfully retrieved data")
+        else:
+            return jsonify(success = False, message = "No such user")
+    else:
+        return jsonify(success = False, message = "You're not signed in")
+
+@app.route("/getusermessagesbytoken", methods=['GET'])
+def get_user_messages_by_token():
+    token = request.values.get('token')
+    user = database_helper.get_user_by_token(token)
+    if len(user) > 0:
+        messages = database_helper.get_messages(user[0][0])
+        if len(messages) > 0:
+            return jsonify(success = True, data = messages, message = "Successfully got user data")
+        else:
+            return jsonify(success = False, message = "User has no messages")
+    else:
+        return jsonify(success = False,  message = "You're not signed in")
+
+
+@app.route("/getusermessagesbyemail", methods=['GET'])
+def get_user_messages_by_email():
+    token = request.values.get('token')
+    email = request.values.get('email')
+    user = database_helper.get_user_by_token(token)
+    if len(user) > 0:
+        messages = database_helper.get_messages(email)
+        if len(messages) > 0:
+            return jsonify(success = True, data = messages, message = "Successfully got user data")
+        else:
+            return jsonify(success = False, message = "User has no messages")
+    else:
+        return jsonify(success = False, message = "You're not signed in")
+
+@app.route("/postmessage", methods=['POST'])
+def post_message():
+    token = request.values.get('token')
+    message = request.values.get('message')
+    reciever = request.values.get('email')
+    sender = database_helper.get_user_by_token(token)
+    if(token != None and message != None and reciever != None):
+        if len(sender) > 0:
+            tmp = database_helper.get_user_by_email(reciever)
+            if len(tmp) > 0:
+                database_helper.post_message(sender[0][0], reciever, message)
+                return jsonify(success = True, message = "Successfully posted message")
+            else:
+                return jsonify(success = False, message = "No such user")
+        else:
+            return jsonify(success = False, message = "You're not signed in")
+    else:
+        return jsonify(success = False, message = "Wrong input data")
+
+
+
