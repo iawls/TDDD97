5abc30b75eb0d226f510102c1c304097bc8e3629
iawls <falken1994@hotmail.com>
2016-03-09 15:54:52 +0100

    Removed servestub

--- Twidder/runserver.py
+++ Twidder/runserver.py
@@ -3,7 +3,7 @@ from geventwebsocket.handler import WebSocketHandler
 from Twidder import app
 
 app.run(debug=True)
-http_server = WSGIServer(('', 5000), app)
+http_server = WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
 http_server.serve_forever()
 
 
