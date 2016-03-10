2375e69323a532a24f2f079ea55fe331daa41205
iawls <falken1994@hotmail.com>
2016-03-10 16:21:57 +0100

    fuck debug mode

--- Twidder/runserver.py
+++ Twidder/runserver.py
@@ -2,8 +2,5 @@ from gevent.wsgi import WSGIServer
 from geventwebsocket.handler import WebSocketHandler
 from Twidder import app
 
-app.run(debug=True)
 http_server = WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
 http_server.serve_forever()
-
-
